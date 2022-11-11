25b3d730f896f7e8fe4fa61f16ce283f124d9b55
wvanvlaenderen <wvanvlaenderen@gmail.com>
2022-11-11 14:23:48 +0100

    example of broken test

--- examples/api-sst-auth-google/services/test/sample.test.ts
+++ examples/api-sst-auth-google/services/test/sample.test.ts
@@ -1,7 +1,25 @@
-import { describe, it, expect } from "vitest";
+import {test} from "vitest";
+import {Session} from '@serverless-stack/node/auth';
+import {ViteStaticSite} from '@serverless-stack/node/site';
 
-describe("sample", () => {
-  it("should work", () => {
-    expect(true).toBe(true);
-  });
+declare module "@serverless-stack/node/auth" {
+    export interface SessionTypes {
+        user: {
+            userID: string;
+        };
+    }
+}
+
+test("should return activities", async () => {
+    try {
+        Session.parameter({
+            redirect: process.env.IS_LOCAL ? "http://127.0.0.1:5173" : ViteStaticSite.site.url,
+            type: "user",
+            properties: {
+                userID: 'testing1234',
+            },
+        });
+    } catch (e) {
+        throw e;
+    }
 });
